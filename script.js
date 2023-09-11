49a94ecbb7bf57f172ec3c982b2dd27d74fff539
Maria Eduarda <108494852+dudamaina@users.noreply.github.com>
2023-09-11 10:47:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+escrever = msg => alert(msg);
+soma = (a,b) => a + b;
+sub = (a,b) => a - b;
+mult = (a,b) => a * b;
+div = (a,b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) => {
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta < 0) return "Não possui raiz Real.";
+    if (delta == 0) return "x1 = x2 = " + div(-b,mult(2,a));
+    return "x1 = " + div (soma(-b,raiz(delta)),mult(2,a)) + 
+           "x2 = " + div (sub(-b,raiz(delta)),mult(2,a));
+}
+
+escrever(equacao2Grau(3,-2,-1));
